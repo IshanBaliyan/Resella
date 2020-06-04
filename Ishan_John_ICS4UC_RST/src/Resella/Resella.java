a866bf201ea5a0a5de9f9a72cae59385053b7f24
Ishan <ishanbaliyan@gmail.com>
2020-06-04 16:37:06 -0400

    Removed constructor from Resella class

--- Ishan_John_ICS4UC_RST/src/Resella/Resella.java
+++ Ishan_John_ICS4UC_RST/src/Resella/Resella.java
@@ -2,10 +2,6 @@ package Resella;
 
 public class Resella {
 
-	public Resella() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

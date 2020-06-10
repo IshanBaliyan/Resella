fea99952ecb0748fc5ec6f99a3d1c28c95c85423
John Wolf <4johnryanwolf@gmail.com>
2020-06-10 12:32:42 -0400

    Integrated kijiji web scraping
    * Also added empty constructor to ProductListing

--- Ishan_John_ICS4UC_RST/src/resella/WebScraper.java
+++ Ishan_John_ICS4UC_RST/src/resella/WebScraper.java
@@ -1,6 +1,5 @@
 package resella;
 
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
@@ -13,17 +12,19 @@ import org.jsoup.select.Elements;
 
 public class WebScraper {
 
-	//Declaring variables for use in class
+	// Declaring variables for use in class
 	private int numListings;
 	private String keywords;
+	private String kijijiSearchID = "k0l9004";
+	private String kijijiLocation = "ontario";
 	private ArrayList<ProductListing> activeAdListings;
 	private ArrayList<ProductListing> soldAdListings;
 
- 
 	/**
 	 * Constructor for adding keywords
 	 * 
-	 * @param keywords	The keywords or the "search text" that the user is using to search the item on the program 
+	 * @param keywords The keywords or the "search text" that the user is using to
+	 *                 search the item on the program
 	 */
 	public WebScraper(String keywords) {
 		this.keywords = keywords;
@@ -32,15 +33,20 @@ public class WebScraper {
 	/**
 	 * Method that scrapes the listings
 	 */
-	public void scrapeListings(){
-		//item results URL declared and assigned to variable from keywords searched by the user
+	public void scrapeListings() {
+		// item results URL declared and assigned to variable from keywords searched by
+		// the user
 		String activeEBayResultsURL = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + keywords + "&_sop=15";
 		activeAdListings.addAll(scrapeSearchResultsEBay(activeEBayResultsURL));
-		
-		String soldEBayResultsURL = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + keywords + "&LH_Sold=1" + "&_ipg=100" + "&_sop=16";
-		soldAdListings.addAll(scrapeSearchResultsEBay(soldEBayResultsURL));
-	}
 
+		String soldEBayResultsURL = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + keywords + "&LH_Sold=1"
+				+ "&_ipg=100" + "&_sop=16";
+		soldAdListings.addAll(scrapeSearchResultsEBay(soldEBayResultsURL));
+
+		String activeKijijiResultsURL = "https://www.kijiji.ca/b-" + kijijiLocation + "/" + keywords + "/"
+				+ kijijiSearchID + "?dc=true" + "&sort=priceAsc";
+		soldAdListings.addAll(scrapeSearchResultsKijiji(activeKijijiResultsURL));
+	}
 
 	/**
 	 * This method scrapes search results on EBay
@@ -57,12 +63,12 @@ public class WebScraper {
 			// Get the list of repositories
 			Elements numResults = doc.getElementsByClass("srp-controls__count-heading");
 
-			//Printing the message for the number of results for the item
+			// Printing the message for the number of results for the item
 			System.out.println(numResults.text());
 			Pattern r = Pattern.compile("^([0-9],?)+");
 			Matcher m = r.matcher(numResults.text());
 
-			if (m.find( )) {
+			if (m.find()) {
 				int numListings = Integer.parseInt(m.group(0).replaceAll(",", ""));
 				System.out.println(numListings);
 				this.numListings += numListings;
@@ -71,8 +77,7 @@ public class WebScraper {
 			// Get the list of repositories
 			Elements searchItems = doc.getElementsByClass("s-item__image");
 
-
-			//Canadian repo code:rsHdr
+			// Canadian repo code:rsHdr
 
 			for (Element searchItem : searchItems) {
 
@@ -81,12 +86,12 @@ public class WebScraper {
 
 					String href = link.attr("href");
 
-					if(!href.isEmpty()) {
+					if (!href.isEmpty()) {
 
 						// get the value from the href attribute
 						System.out.println("\nlink: " + href);
 						listingSearchResults.add(scrapeListingEBay(href));
-						
+
 					}
 				}
 
@@ -105,34 +110,32 @@ public class WebScraper {
 	 * @param productURL The URL for the product listing
 	 */
 	private ProductListing scrapeListingEBay(String productURL) {
-		
+
 		productURL = "https://www.ebay.com/itm/2020-Chevrolet-Corvette-Stingray-Z51-LT1/303583464119?hash=item46aefc0ab7:g:sPIAAOSwNU9e0~AS";
-		
+
 		try {
 			// Here we create a document object and use JSoup to fetch the website
 			Document doc = Jsoup.connect(productURL).get();
 
 			Elements titleElement = doc.getElementsByClass("it-ttl");
-			String title = doc.getElementsByClass("srp-controls__count-heading").text().replaceFirst("Details about ", "");
-			
+			String title = doc.getElementsByClass("srp-controls__count-heading").text().replaceFirst("Details about ",
+					"");
+
 			Elements priceElement = doc.getElementsByClass("vi-mskumap-none");
-			int price = Integer.parseInt(doc.getElementsByClass("srp-controls__count-heading").text().replaceFirst("Details about ", ""));
-			
+			int price = Integer.parseInt(
+					doc.getElementsByClass("srp-controls__count-heading").text().replaceFirst("Details about ", ""));
+
 			System.out.println("Item name: " + title);
 			System.out.println("Item price: " + price);
-			
-			//img url, listingtype (auction or buyitnow)
-			//description (filter keywords - broken, needs repair, etc..)
-			//
-			
 
-			
-			
-			
+			// img url, listingtype (auction or buyitnow)
+			// description (filter keywords - broken, needs repair, etc..)
+			//
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
 
@@ -140,11 +143,48 @@ public class WebScraper {
 	 * This method scrapes search results from kjiji
 	 * 
 	 * @param searchURL The search URL for the user's keywords
+	 * @return 
 	 */
-	private void scrapeSearchResultsKijiji(String searchURL) {
+	private ArrayList<ProductListing> scrapeSearchResultsKijiji(String searchURL) {
+		ArrayList<ProductListing> listingSearchResults = new ArrayList<ProductListing>();
+		// TODO: check what ?dc=true means
+		// Here we create a document object and use JSoup to fetch the website
+		Document doc;
+		try {
+			doc = Jsoup.connect(searchURL).get();
 
-		
+			// Get the list of repositories
+			Elements numMatches = doc.getElementsByClass("showing");
 
+			System.out.println(numMatches.text());
+			Pattern r = Pattern.compile("([0-9],?)+ Ad");
+			Matcher m = r.matcher(numMatches.text());
+			if (m.find()) {
+				System.out.println(m.group(0).replaceAll("[ Ad,]", ""));
+			}
+
+			// Get the list of repositories
+			Elements searchItems = doc.getElementsByClass("regular-ad");
+
+			for (Element searchItem : searchItems) {
+
+				Elements links = searchItem.select("a[href]");
+				for (Element link : links) {
+
+					String href = link.attr("href");
+
+					if (!href.isEmpty()) {
+
+						// get the value from the href attribute
+						listingSearchResults.add(scrapeListingKijiji("https://www.kijiji.ca" + href));
+					}
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return listingSearchResults;
 	}
 
 	/**
@@ -152,22 +192,45 @@ public class WebScraper {
 	 * 
 	 * @param productURL The URL for the product listing
 	 */
-	private void scrapeListingKijiji(String productURL) {
+	private ProductListing scrapeListingKijiji(String productURL) {
+		ProductListing scrapedListing = new ProductListing();
+		ArrayList<String> tags = new ArrayList<String>();
+		try {
+			Document listing = Jsoup.connect(productURL).get();
 
+			// Scrape title:
+			Element titleELement = listing.getElementsByClass("title-2323565163").first();
+			String title = titleELement.text();
 
+			// Scrape price:
+			Element priceElement = listing.getElementsByClass("currentPrice-2842943473").first();
+			String priceStr = priceElement.text().replaceAll("[\\$,]", "");
+			double price = Double.parseDouble(priceStr);
+
+			// Scrape image URL
+			Element presentation = listing.getElementById("mainHeroImage");
+			Element image = presentation.getElementsByTag("img").first();
+			String imageURL = image.attr("src");
+			
+			// Create scrapedListing
+			scrapedListing = new ProductListing(imageURL, price, title, productURL,
+					ProductListing.BUY_IT_NOW_LISTING, ProductListing.KIJIJI, tags);
+
+			// In case of any IO errors, we want the messages written to the console
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return scrapedListing;
 	}
 
+	// private ArrayList<ProductListing> getActiveAdListings() {
+	//
+	//
+	// }
 
-	//	private ArrayList<ProductListing> getActiveAdListings() {
-	//		
-	//		
-	//	}
-
-	//	private ArrayList<ProductListing> getSoldAdListings() {
-	//	
-	//	
-	//}
-
-
+	// private ArrayList<ProductListing> getSoldAdListings() {
+	//
+	//
+	// }
 
 }
